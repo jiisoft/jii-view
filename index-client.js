a42de3ea4588ff78a3f91ea27e85508b234d869b
Kozhin Vladimir <affka@affka.ru>
2016-01-01 21:46:09 +0700

    remove themes, add react view, change renderers logic

--- index-client.js
+++ index-client.js
@@ -1,8 +1,17 @@
 module.exports = require('jii');
 
-require('./lib/Theme.js');
+// AUTO-GENERATED Package files list
+require('./lib/IRenderer.js');
 require('./lib/View.js');
 require('./lib/ViewEvent.js');
-require('./lib/ViewRenderer.js');
 require('./lib/WebView.js');
 require('./lib/client/ClientWebView.js');
+require('./lib/react/LayoutView.js');
+require('./lib/react/ReactRenderer.js');
+require('./lib/react/ReactView.js');
+require('./lib/react/form/ActiveForm.js');
+require('./lib/react/form/Input.js');
+require('./lib/react/grid/Column.js');
+require('./lib/react/grid/DataColumn.js');
+require('./lib/react/grid/GridView.js');
+require('./lib/react/widgets/BaseListView.js');
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 module.exports = require('jii');
 
-require('./lib/Theme.js');
+require('node-jsx').install();
+
+// AUTO-GENERATED Package files list
+require('./lib/IRenderer.js');
 require('./lib/View.js');
 require('./lib/ViewEvent.js');
-require('./lib/ViewRenderer.js');
 require('./lib/WebView.js');
+require('./lib/react/LayoutView.js');
+require('./lib/react/ReactRenderer.js');
+require('./lib/react/ReactView.js');
+require('./lib/react/form/ActiveForm.js');
+require('./lib/react/form/Input.js');
+require('./lib/react/grid/Column.js');
+require('./lib/react/grid/DataColumn.js');
+require('./lib/react/grid/GridView.js');
+require('./lib/react/server/ReactServerRenderer.js');
+require('./lib/react/widgets/BaseListView.js');
 require('./lib/server/ServerWebView.js');
+require('./lib/server/underscore/UnderscoreRenderer.js');
